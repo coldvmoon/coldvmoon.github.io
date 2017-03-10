3d3da9ce925c0f43dbd6193300431dd46943d8e4
xkw <xkwu1990@gmail.com>
2017-03-10 17:40:57 +0800

    add vivus.js

---
+++ _drafts/auto/2017-03-08-sumary-of-recent-work.el
@@ -0,0 +1,43 @@
+(TeX-add-style-hook
+ "2017-03-08-sumary-of-recent-work"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:orge633d17"
+    "sec:org361e2f3"
+    "sec:org97b1ffb"
+    "sec:org6eacff1"
+    "sec:org686d572"
+    "sec:orgdb0e80e"
+    "sec:orga758aca"
+    "sec:org9a5dfb2"
+    "sec:org601522a"))
+ :latex)
+
